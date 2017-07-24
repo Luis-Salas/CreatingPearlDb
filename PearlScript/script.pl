32580e7b86ad308fcfe24ea69d708e44b3c2ffe6
Luis <salas.luis40@gmail.com>
2017-07-24 18:14:12 -0500

    adding comments to code

--- PearlScript/script.pl
+++ PearlScript/script.pl
@@ -17,15 +17,17 @@ my $db = DBI->connect("dbi:SQLite:test1.db") or die "couldn’t connect to db".D
 $db->do("CREATE TABLE IF NOT EXISTS user (id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, home TEXT)");
 
 foreach my $elem (@list) {
+  # loops trough our pearl object and inserts every value into the collum of the table
   $db->do("INSERT INTO user\(first_name, last_name, home) VALUES ( '$elem->{first_name}','$elem->{last_name}','$elem->{home}')");
 }
-
+# Selects  all of the items in the User table that we created
 my $all = $db->selectall_arrayref("SELECT * FROM USER");
+# Loops trough out object that we have in our variable and prints out every user inputted to the database
 
 foreach my $row (@$all) {
 my ($id, $first_name, $last_name, $home) = @$row;
 print "$id,$first_name,$last_name, $home";
 
 }
-
+# disconnects from the database
 $db->disconnect;
